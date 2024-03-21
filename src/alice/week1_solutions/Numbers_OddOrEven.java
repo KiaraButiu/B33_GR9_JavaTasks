cdc98956d98e03223e869542dfe3244f4c098ffe
aliceyarema <yarema.alice@yahoo.com>
2024-03-21 13:55:07 -0400

    updated

--- src/alice/week1_solutions/Numbers_OddOrEven.java
+++ src/alice/week1_solutions/Numbers_OddOrEven.java
@@ -2,15 +2,20 @@ package alice.week1_solutions;
 
 public class Numbers_OddOrEven {
 
+    public static void main(String[] args) {
+        checkOddEven(7);
+
+    }
+
     public static void checkOddEven(int number) {
 
         //modulo operator (%), which gives the remainder of a division operation
         //If the remainder is 0 when divided by 2, then the number is even
 
         if(number % 2 == 0) {
-            System.out.println(number + "even");
+            System.out.println(number+ " even");
         } else {
-            System.out.println(number + "odd");
+            System.out.println(number + " odd");
         }
     }
 

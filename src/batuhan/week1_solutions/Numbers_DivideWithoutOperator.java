f93785dfe94f60fecf45dc7c0d9e336ddc843fad
Batuhan Balkilic <batuhanbalkilic@hotmail.com>
2024-03-21 13:53:49 -0400

    Week1 tasks are completed.

--- src/batuhan/week1_solutions/Numbers_DivideWithoutOperator.java
+++ src/batuhan/week1_solutions/Numbers_DivideWithoutOperator.java
@@ -1,4 +1,23 @@
 package batuhan.week1_solutions;
 
 public class Numbers_DivideWithoutOperator {
+    /*
+  Write a method that can divide two numbers without using division operator
+   */
+
+
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 2;
+        System.out.println(divide(dividend, divisor));
+    }
+
+    public static int divide(int dividend, int divisor) {
+        int quotient = 0;
+        while (dividend >= divisor) {
+            dividend -= divisor;
+            quotient++;
+        }
+        return quotient;
+    }
 }

a84892df3388ef754a247462cad257c5b4e4524f
seymaSu <seymasonmez14@gmail.com>
2024-03-25 11:30:55 -0400

    Week 1 tasks are done!

--- src/seyma/week1_solutions/Numbers_OddOrEven.java
+++ src/seyma/week1_solutions/Numbers_OddOrEven.java
@@ -1,4 +1,28 @@
 package seyma.week1_solutions;
 
+import java.util.Scanner;
+
 public class Numbers_OddOrEven {
+
+    public static void main(String[] args) {
+
+        oddOrEven(26);
+    }
+    public static void oddOrEven(int num){
+        if (num%2==0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+
+    /*
+    Write a method which can identifies given number is even or odd
+        EX:
+        identify(5) ->  "Odd"
+        identify(6) ->  "Even"
+
+     */
+
+
 }

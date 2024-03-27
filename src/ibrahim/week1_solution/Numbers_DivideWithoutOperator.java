71aff103696230a5bf463ed431df1dcba776da0c
ibrahimibrahim <ibrahimib1201@gmail.com>
2024-03-27 09:21:34 -0400

    Hello

--- src/ibrahim/week1_solution/Numbers_DivideWithoutOperator.java
+++ src/ibrahim/week1_solution/Numbers_DivideWithoutOperator.java
@@ -3,7 +3,7 @@ package ibrahim.week1_solution;
 public class Numbers_DivideWithoutOperator {
     public static void main(String[] args) {
 
-
+        System.out.println("Hello");
     }
      /*
     Write a method that can divide two numbers without using division operator

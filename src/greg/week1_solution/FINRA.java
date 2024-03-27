bfc73ec5bee4687af3005fb5e9592470c4039c8e
GregoryKopylov <gregorykopylov@yahoo.com>
2024-03-27 17:24:47 -0400

    update : change to print it in a lane

--- src/greg/week1_solution/FINRA.java
+++ src/greg/week1_solution/FINRA.java
@@ -6,20 +6,20 @@ public class FINRA {
 
         for (int i = 1; i <= 30; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
-                System.out.println("FINRA");
+                System.out.print("FINRA ");
             } else if (i % 3 == 0) {
-                System.out.println("FIN");
+                System.out.print("FIN ");
             } else if (i % 5 == 0) {
-                System.out.println("RA");
+                System.out.print("RA ");
             } else {
-                System.out.println(i);
+                System.out.print(i + " ");
             }
         }
     }
 }
     /*
 
-    ChANGE NUMBER 1
+    CHANGE NUMBER 1
     FINRA :
 Write a method which prints out the numbers from 1 to 30 but for numbers which
 are a multiple of 3 print "FIN" instead of the number and for numbers which are a

35cd9980b306df40a947c02caa392982885bf731
fatouma <madani.fatouma@gmail.com>
2024-03-26 22:28:02 -0400

    Test DivideWithoutOperator

--- src/Fettouma/Week1_Solutions/Numbers_DivideWithoutOperator.java
+++ src/Fettouma/Week1_Solutions/Numbers_DivideWithoutOperator.java
@@ -1,6 +1,22 @@
 package Fettouma.Week1_Solutions;
 
 public class Numbers_DivideWithoutOperator {
+
+    public static void main(String[] args) {
+      DivideWithoutOperator(21,5);
+    }
+  public static void DivideWithoutOperator(double num1, double num2){
+
+      double rest=0;
+    double count = 0;
+
+        while (num1 >= num2){
+        num1 -= num2;
+        count++;
+
+    }
+      System.out.println(count);
+}
 //
      /*
     Write a method that can divide two numbers without using division operator

bb9de6014bb5f548b62b287886fe306b92b0721a
irinayarema <MykolaYarema@yahoo.com>
2024-03-30 17:38:05 -0400

    week 2 updated

--- src/Irina/week1_solution/Numbers_DivideWithoutOperator.java
+++ src/Irina/week1_solution/Numbers_DivideWithoutOperator.java
@@ -1,6 +1,6 @@
 package Irina.week1_solution;
 
-public class Numbers_DivideWithoutOperator {
+public class Numbers_DivideWithoutOperator { // my goal make the method visible. 
 
     public static int divide(int dividend, int divisor) {
         int quotient = 0;
@@ -20,7 +20,6 @@ public class Numbers_DivideWithoutOperator {
         int quotient = divide(dividend, divisor);
         System.out.println("Quotient: " + quotient);
     }
-
 
 }
 
---
+++ src/Irina/week2_solutions/Number_SwapTwoNumbers.java
@@ -0,0 +1,29 @@
+package Irina.week2_solutions;
+
+public class Number_SwapTwoNumbers {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        swap(a, b);
+    }
+
+    public static void swap(int a, int b) {
+        a = a + b; // sum stored in 'a'
+        b = a - b; // 'b' becomes original 'a'
+        a = a - b; // 'a' becomes original 'b'
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+
+}
+
+    /*
+    Swap two variable values without using a third variable
+     */
+
+
+
+
+
+

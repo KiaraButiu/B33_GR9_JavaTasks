a4e4adec6113790dc60377893204d90b5b66fcb2
GregoryKopylov <gregorykopylov@yahoo.com>
2024-04-03 15:52:13 -0400

    update : task 3 is resolved

--- src/greg/week1_solution/Numbers_DivideWithoutOperator.java
+++
@@ -1,9 +0,0 @@
-package greg.week1_solution;
-
-public class Numbers_DivideWithoutOperator {
-
-}
-
-/*
-    Write a method that can divide two numbers without using division operator
-     */
--- src/greg/week1_solution/Numbers_OddOrEven.java
+++ src/greg/week1_solution/Numbers_OddOrEven.java
@@ -2,7 +2,23 @@ package greg.week1_solution;
 
 public class Numbers_OddOrEven {
 
-}
+    public static void main(String[] args) {
+        // Test cases
+        System.out.println("identify(5) = " + identify(5)); // Odd
+        System.out.println("identify(6) = " + identify(6)); // Even
+        System.out.println("identify(7) = " + identify(7)); // Odd
+
+    }
+     public static String identify ( int number){
+
+            if (number % 2 == 0) {
+                return "Even";
+            }else{
+                return "Odd";
+            }
+        }
+    }
+
 
 /*
     Write  a method which can identifies given number is even or odd

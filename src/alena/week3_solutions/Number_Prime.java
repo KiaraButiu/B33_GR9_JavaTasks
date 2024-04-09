d536dbcaf9d72659898d8b9988f9ec31ecdc77c5
ICELENAM <elenflowe23@gmail.com>
2024-04-09 14:15:03 -0400

    week3 solutions prime number

--- src/alena/week3_solutions/Number_Prime.java
+++ src/alena/week3_solutions/Number_Prime.java
@@ -1,11 +1,23 @@
 package alena.week3_solutions;
 
 public class Number_Prime {
+    public static void main(String[] args) {
+        int num = 103;
 
-    /*
+        for (int i = 2; i < num; i++) {
+
+
+            if (num % i == 0) {
+                System.out.println(num + " is a not prime number");
+                return;
+
+            }
+
+        }
+        System.out.println(num + " a prime number.");
+    }
+}
+/*
         Write a method that can check if a number is prime or not
      */
 
-
-
-}

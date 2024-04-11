4b1dc3aebddc143c46fb43cf7518a92a02a8bee0
TroyYC <troy@cydeo.com>
2024-04-11 15:03:34 -0400

    week4 tasks added

---
+++ src/mentor/week4_tasks/String_FrequencyOfCharacters.java
@@ -0,0 +1,25 @@
+package mentor.week4_tasks;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+
+public class String_FrequencyOfCharacters {
+
+    /*
+    Write function that can find the frequency of characters
+            Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+     */
+    public static void main(String[] args) {
+        String str = "aaabbbcccdddEEEFFF";
+
+
+
+
+
+    }
+
+
+
+
+}

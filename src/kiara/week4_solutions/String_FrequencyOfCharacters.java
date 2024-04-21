b6fd14d14cbb120fd9ece99825261e96a8d3ea3a
KiaraButiu <kikilorena49@yahoo.com>
2024-04-20 22:29:06 -0400

    Week5 Task Solution Completed

--- src/kiara/week4_solutions/String_FrequencyOfCharacters.java
+++ src/kiara/week4_solutions/String_FrequencyOfCharacters.java
@@ -14,7 +14,7 @@ public class String_FrequencyOfCharacters {
         for (int i = 0; i < str.length() ; i++) {     // Outer Loop : iterates through each character of String "str"
                                                            // Inside the loop :
             char ch = str.charAt(i);                           // gets the character of current index 'i' & stores it in variable 'ch'
-            int count = 0;                                     // variable 'count' will count the frequency of current character
+            int count = 0;                                     // variable 'count' will store the frequency of current character
 
             for (int j = 0; j < str.length(); j++) {    // Inner Loop : iterates through each character of String "str" AGAIN
                                                             // this loop compares each character of the string with character 'ch' obtained

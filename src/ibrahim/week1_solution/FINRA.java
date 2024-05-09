1ca8b9ab387884bd40b8b113fec7bd8145b59211
ibrahimibrahim <ibrahimib1201@gmail.com>
2024-05-09 12:37:31 -0400

    Hello test 3 123

--- src/ibrahim/week1_solution/FINRA.java
+++ src/ibrahim/week1_solution/FINRA.java
@@ -3,16 +3,23 @@ package ibrahim.week1_solution;
 public class FINRA {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello world");
+        finra();
     }
 
-    /*
-    FINRA :
-Write
-a method which prints out the numbers from 1 to 30 but for numbers which
-are a multiple of 3 print "FIN" instead of the number and for numbers which are a
-multiple of 5, print "RA" instead of the number. for numbers which are a multiple of
-both 3 and 5, print "FINRA" instead of the number.
-     */
+    public static void finra() {
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print(" FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print(" FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print(" RA ");
+            } else {
+                System.out.print(i + " ");
+
+            }
+        }
+    }
+
 }
--- src/ibrahim/week1_solution/Numbers_OddOrEven.java
+++ src/ibrahim/week1_solution/Numbers_OddOrEven.java
@@ -2,11 +2,16 @@ package ibrahim.week1_solution;
 
 public class Numbers_OddOrEven {
 
-    /*
-    Write  a method which can identifies given number is even or odd
-        EX:
-        identify(5) ->  "Odd"
-        identify(6) ->  "Even"
 
-     */
+    public static void main(String[] args) {
+        numberChecker(5);
+    }
+
+    public static void numberChecker (int number){// This method takes number as an argument
+        if (number % 2 != 0){                     // If number is not divisible by 2 means that this number is ODD
+            System.out.println("This number: " + number + " is ODD");
+        } else {                                  // Otherwise, number is Even
+            System.out.println("This number: " + number + " is EVEN");
+        }
+    }
 }

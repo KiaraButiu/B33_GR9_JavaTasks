4bc117cb91e7f00468e10e5f1dcd618a9adc34d9
TroyYC <troy@cydeo.com>
2024-06-25 10:35:34 -0400

    optional tasks added

---
+++ src/mentor/optional_tasks/Array_Permutation.java
@@ -0,0 +1,22 @@
+package mentor.optional_tasks;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Array_Permutation {
+
+   /*
+    Given an array of 3 characters print all permutation combinations from the given characters
+    input: char[] ch = {'a', 'b', 'c'};
+    output:     [a, b, c]
+                [a, c, b]
+                [b, a, c]
+                [b, c, a]
+                [c, a, b]
+                [c, b, a]
+    */
+
+
+
+}
